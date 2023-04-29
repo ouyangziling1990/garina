d6d77705828cbe89c833cac0c2573179a349e11f
wujingwei1990 <wujingwei1990@126.com>
2023-04-29 16:10:38 +0800

    feat: 增加footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Footer">
+    关于
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  beforeCreate() {},
+  created() {},
+  mounted() {},
+  computed: {
+    ...mapState([]),
+  },
+  watch: {},
+  methods: {},
+}
+</script>
+<style lang="less" scoped>
+.Footer {
+  height: 40px;
+  background-color: rgba(215, 217, 215, 0.2);
+  padding: 10px;
+}
+</style>
