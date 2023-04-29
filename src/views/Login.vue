9560f9439999120fa279a82dee269c05558f4538
wujingwei1990 <wujingwei1990@126.com>
2023-04-29 15:58:50 +0800

    fix: login 提示去掉

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -132,7 +132,7 @@ export default {
           if (accessToken) {
             this.accessToken = accessToken;
             localStorage.setItem("access_token", accessToken);
-            this.$message.info("登录成功");
+            // this.$message.info("登录成功");
 
             await this.getUserInfo();
             this.goBack()
